9f44784ea89c56e5a522aec67122daf291a27df2
Linus Apelgren <linusapelgren6789@gmail.com>
2024-04-16 14:21:35 +0000

    Cleaned up code

---
+++ assets/scripts/game.js
@@ -0,0 +1,232 @@
+const WEAPONS_CONFIG = {
+    rock: {
+        name: 'Rock',
+        winsOver: ['scissors', 'lizard'],
+    },
+    paper: {
+        name: 'Paper',
+        winsOver: ['rock', 'spock'],
+    },
+    scissors: {
+        name: 'Scissors',
+        winsOver: ['paper', 'lizard'],
+    },
+    lizard: {
+        name: 'Lizard',
+        winsOver:  ['spock', 'paper'],
+    },
+    spock: {
+        name: 'Spock',
+        winsOver: ['rock', 'scissors']
+    },
+};
+
+// Initialize the scores
+let scores = {
+    player: 0,
+    computer: 0
+};
+
+let isWeaponClickDisabled = false;
+
+// Get the start screen and the game screen
+let startScreen = document.querySelector('.startGameWindow');
+let gameScreen = document.querySelector('.gameWindow');
+
+// Get game music
+let gameMusic = new Audio('./assets/sound/GameMusic.mp3');
+let winSound = new Audio('./assets/sound/yay.mp3');
+
+//Get start button
+let startButton = document.querySelector('#startGame');
+
+function onStartBtnClick() {
+    // Hide the start screen and show the game screen
+    startScreen.style.display = 'none';
+    gameScreen.style.display = 'block';
+    gameMusic.loop = true;
+    gameMusic.play();
+    gameMusic.volume = 0.5;
+}
+
+// Function to get the computer's choice
+function getComputerChoice() {
+    var keys = Object.keys(WEAPONS_CONFIG);    
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    return WEAPONS_CONFIG[keys[randomIndex]].name.toLowerCase();
+}
+
+// Function to determine the winner
+function getWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return {playerWon: false, message: 'It\'s a tie!'};
+    } else if (WEAPONS_CONFIG[playerChoice].winsOver.includes(computerChoice)) {
+        scores.player++;
+        return {playerWon: true, message: 'Player wins!'};
+    } else {
+        scores.computer++;
+        return {playerWon: false, message: 'Computer wins!'};
+    }
+}
+
+// Get the buttons
+let buttons = document.querySelectorAll('.moveList button');
+
+// Get the scoreboard
+let scoreboard = {
+    player: document.querySelector('#playerScore'),
+    computer: document.querySelector('#opponentScore')
+};
+
+// Get the screenOverlay div
+let screenOverlay = document.querySelector('.screenOverlay');
+
+// Create a new element for the result
+let resultElement = document.createElement('p');
+resultElement.id = 'result'; 
+screenOverlay.appendChild(resultElement);
+
+function onWeaponBtnClick() {
+
+    if(isWeaponClickDisabled) {
+        return;
+    }
+
+    isWeaponClickDisabled = true;
+    // Get the player's hand and the opponent's hand images
+    let playerHandImg = document.querySelector('.playerHandImg img');
+    let opponentHandImg = document.querySelector('.opponentHandImg img');
+
+    // Reset the player and opponent hand images
+    playerHandImg.src = "./assets/screenshots/moves/emptyhand.png";
+    opponentHandImg.src = "./assets/screenshots/moves/emptyhand.png";
+
+    // Apply the fade animation to the images
+    playerHandImg.classList.add('fade');
+    opponentHandImg.classList.add('fade');
+
+    resultElement.style.display = 'none'; // Hide the result element
+    
+    let playerChoice = this.id.toLowerCase(); // The id of the button is the player's choice
+
+    // Delay the execution of the move by 3 seconds
+    setTimeout(function() {
+        // Remove the fade animation from the images
+        playerHandImg.classList.remove('fade');
+        opponentHandImg.classList.remove('fade');
+
+        // Play the game
+        let computerChoice = getComputerChoice();
+        let {playerWon, message} = getWinner(playerChoice, computerChoice);
+
+        // Update the scores in localStorage
+        localStorage.setItem('scores', JSON.stringify(scores));
+
+        console.log(`Player chose ${playerChoice}`);
+        console.log(`Computer chose ${computerChoice}`);
+        console.log(message);
+
+        // Set the src attribute of the img elements
+        playerHandImg.src = "./assets/screenshots/moves/" + playerChoice + ".png";
+        opponentHandImg.src = "./assets/screenshots/moves/" + computerChoice + ".png";
+
+        // Display the result in the middle of the screen
+        resultElement.textContent = message;
+        resultElement.style.display = 'block';
+
+        // Update the scoreboard
+        scoreboard.player.textContent = "Player: " + scores.player;
+        scoreboard.computer.textContent = "Computer: " + scores.computer;
+        
+        // Play the round win sound if the player or the computer wins the round
+        let roundWin = new Audio('./assets/Sound/roundWin.mp3');
+        if ((playerWon && scores.player < 3) || (!playerWon && scores.computer < 3)) {
+            gameMusic.volume = 0.2; // Lower the volume of the game music;
+            roundWin.play();
+
+            // Resume the game music when the round win sound has finished playing
+            roundWin.onloadedmetadata = function() {
+                setTimeout(function() {
+                    gameMusic.volume = 0.5; // Reset the volume of the game music
+                }, roundWin.duration * 1000); // roundWin.duration is in seconds, so multiply by 1000 to convert to milliseconds
+            };
+        }
+        // Check if the game is over
+        if (scores.player === 3 || scores.computer === 3) {
+            // Pause the game music
+            gameMusic.pause();
+            winSound.play();
+
+            // Update the result to show who won the game
+            if (scores.player === 3) {
+                resultElement.textContent = 'Player wins the game!';
+            } else {
+                resultElement.textContent = 'Computer wins the game!';
+            }
+
+            // Clear the game screen and show the start screen after a delay
+            setTimeout(function() {
+                // Hide the gameWindow div
+                gameScreen.style.display = 'none';
+
+                // Show the startGameWindow and the restart button
+                startScreen.style.display = 'block';
+                restartButton.style.display = 'block';
+
+                // Hide the startButton
+                startButton.style.display = 'none';
+            }, 3500); // 3500 milliseconds = 3.5 seconds
+        }
+
+        isWeaponClickDisabled = false
+    }, 3000); // 3000 milliseconds = 3 seconds 
+}
+
+// Get restart button
+let restartButton = document.querySelector('#restartGame');
+//Get empty hand image
+let emptyHandImg =  document.querySelector('.emptyHandImg');
+
+
+function onRestart() {
+    // Hide the start screen and show the game screen
+    startScreen.style.display = 'none';
+    gameScreen.style.display = 'block';
+    gameMusic.loop = true;
+    gameMusic.currentTime = 0;
+    gameMusic.play();
+    gameMusic.volume = 0.5;
+    restartButton.style.display = 'none';
+    // Reset the game
+    resetGame();
+}
+
+
+// Function to reset the game
+function resetGame() {
+    // Reset the game
+    scores.player = 0; // Reset the player score
+    scores.computer = 0; // Reset the computer score
+    gameMusic.play();
+
+    // Update the scoreboard
+    scoreboard.player.textContent = "Player: " + scores.player;
+    scoreboard.computer.textContent = "Computer: " + scores.computer;
+    // Hide the result element
+    resultElement.style.display = 'none';
+}
+
+function initEventListeners() {
+    // Add a click event listener to the start button
+    startButton.addEventListener('click', onStartBtnClick);    
+
+    // Add a click event listener to the restart button
+    restartButton.addEventListener('click', onRestart);
+
+    // Add a click event listener to each button
+    buttons.forEach(button => {
+        button.addEventListener('click', onWeaponBtnClick); 
+    });
+}
+
+initEventListeners();
