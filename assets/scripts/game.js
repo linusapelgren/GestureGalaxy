eb32328dc0f3e2d1154afe80ca0bdaeaa1d59dcd
linusapelgren <70014475+linusapelgren@users.noreply.github.com>
2024-04-17 14:39:31 +0200

    Added leaderboard and name entry after game

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,5 +1,5 @@
 
-//Variables
+//Variables and Constants
 const WEAPONS_CONFIG = {
     rock: {
         name: 'Rock',
@@ -40,7 +40,9 @@ let scoreboard = {
 let screenOverlay = document.querySelector('.screenOverlay');
 let resultElement = document.createElement('p');
 resultElement.id = 'result'; 
-screenOverlay.appendChild(resultElement);
+if (screenOverlay) {
+    screenOverlay.appendChild(resultElement);
+}
 let restartButton = document.querySelector('#restartGame');
 let emptyHandImg =  document.querySelector('.emptyHandImg');
 let muteButton = document.querySelector('#mute');
@@ -48,6 +50,14 @@ let isMuted = false;
 let roundWin = new Audio('./assets/Sound/roundWin.mp3');
 let arcadeName = document.querySelector('#arcadeName');
 let inputName = document.querySelector('#inputName');
+let leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
+let playerName = '';
+let playerEntry = leaderboard.find(entry => entry.name === playerName);
+let tableBody = document.querySelector('.leaderboardsTable table > tbody');
+let opponentHandImg 
+let playerHandImg 
+let playerChoice
+let move 
 //Functions
 
 // Function to handle the start button click event
@@ -86,12 +96,12 @@ function onWeaponBtnClick() {
 
     isWeaponClickDisabled = true;
 
-    let move = document.getElementById('move');
+    move = document.getElementById('move');
     move.textContent = "Move in progress..."
 
     // Get the player's hand and the opponent's hand images
-    let playerHandImg = document.querySelector('.playerHandImg img');
-    let opponentHandImg = document.querySelector('.opponentHandImg img');
+    playerHandImg = document.querySelector('.playerHandImg img');
+    opponentHandImg = document.querySelector('.opponentHandImg img');
 
     // Reset the player and opponent hand images
     playerHandImg.src = "./assets/screenshots/moves/emptyhand.png";
@@ -103,7 +113,7 @@ function onWeaponBtnClick() {
 
     resultElement.style.display = 'none'; // Hide the result element
     
-    let playerChoice = this.id.toLowerCase(); // The id of the button is the player's choice
+    playerChoice = this.id.toLowerCase(); // The id of the button is the player's choice
 
     // Delay the execution of the move by 3 seconds
     setTimeout(function() {
@@ -190,51 +200,34 @@ function onWeaponBtnClick() {
                     // Update the scoreboard
                     scoreboard.player.textContent = "Player: " + scores.player;
                     scoreboard.computer.textContent = "Computer: " + scores.computer;
-                    
-                    // Play the round win sound if the player or the computer wins the round
-                    roundWin.volume = 0.5;
-                    if ((playerWon && scores.player < 3) || (!playerWon && scores.computer < 3)) {
-                        gameMusic.volume = 0.1; // Lower the volume of the game music;
-                        roundWin.play();
-            
-                        // Resume the game music when the round win sound has finished playing
-                        roundWin.onloadedmetadata = function() {
-                            setTimeout(function() {
-                                gameMusic.volume = 0.2; // Reset the volume of the game music
-                            }, roundWin.duration * 1000); // roundWin.duration is in seconds, so multiply by 1000 to convert to milliseconds
-                        };
-                    }
-                    // Check if the game is over
-                    if (scores.player === 3 || scores.computer === 3) {
-                        // Pause the game music
-                        gameMusic.pause();
-                        winSound.volume = 0.5;
-                        winSound.play();
-            
-                        // Update the result to show who won the game
-                        if (scores.player === 3) {
-                            resultElement.textContent = 'Player wins the game!';
-                        } else {
-                            resultElement.textContent = 'Computer wins the game!';
-                        }
-                    }
-                    move.textContent = "Make a move!";
-                    isWeaponClickDisabled = false;
                 }, 1500); // 1500 milliseconds = 1.5 seconds;
             }
+
+            // Play the round win sound if the player or the computer wins the round
+            roundWin.volume = 0.5;
+            if ((playerWon && scores.player < 3) && playerChoice !== computerChoice || (!playerWon && scores.computer < 3  && playerChoice !== computerChoice)) {
+                gameMusic.volume = 0.1; // Lower the volume of the game music;
+                roundWin.load(); // Ensure the audio file is loaded
+                roundWin.play();
+                console.log('Round win sound played');
+
+                // Resume the game music when the round win sound has finished playing
+                roundWin.onended = function() {
+                    gameMusic.volume = 0.2; // Reset the volume of the game music
+                };
+            }
             // Check if the game is over
-            if (scores.player === 1 || scores.computer === 1) {
+            if (scores.player === 3 || scores.computer === 3) {
                 // Pause the game music
                 gameMusic.pause();
+                winSound.volume = 0.5;
                 winSound.play();
-
-            // Update the result to show who won the game
-            if (scores.player === 3) {
-                resultElement.textContent = 'Player wins the game!';
-            } else {
-                resultElement.textContent = 'Computer wins the game!';
-            }
-
+                // Update the result to show who won the game
+                if (scores.player === 3) {
+                    resultElement.textContent = 'Player wins the game!';
+                } else {
+                    resultElement.textContent = 'Computer wins the game!';
+                }
             // Clear the game screen and show the start screen after a delay
             setTimeout(function() {
                 // Hide the gameWindow div
@@ -253,13 +246,19 @@ function onWeaponBtnClick() {
                     restartButton.style.display = 'block';
                 }
 
-                arcadeName.oninput = function() {
+                arcadeName.onchange = function() {
                     if (arcadeName.value.length === 3) {
                         setTimeout(function() {
                             arcadeName.style.display = 'none';
                             inputName.style.display = 'none';
                             restartButton.style.display = 'block';
                         }, 1000); // 1000 milliseconds = 1 second
+                
+                        // Get the player's name from the arcadeName input field
+                        playerName = this.value;
+                        // Update the leaderboard
+                        console.log('Input event triggered. Current value:', this.value);
+                        updateLeaderboard(playerName);
                     }
                 };
             }, 3500); // 3500 milliseconds = 3.5 seconds
@@ -267,6 +266,50 @@ function onWeaponBtnClick() {
         move.textContent = "Make a move!";
         isWeaponClickDisabled = false;
     }, 1500); // 3000 milliseconds = 3 seconds 
+}
+
+//Function to display the leaderboard
+function displayLeaderboard() {
+    console.log('Displaying leaderboard:', leaderboard);
+
+    // Create a new row for each entry in the leaderboard
+    leaderboard.forEach(entry => {
+        let row = document.createElement('tr');
+
+        let nameCell = document.createElement('td');
+        nameCell.textContent = entry.name.toUpperCase();
+        row.appendChild(nameCell);
+
+        let scoreCell = document.createElement('td');
+        scoreCell.textContent = entry.score;
+        row.appendChild(scoreCell);
+
+        tableBody.appendChild(row);
+    });
+}
+
+//Function to update the leaderboard
+function updateLeaderboard(playerName) {
+    playerName = playerName.toUpperCase();
+    // Find the player in the leaderboard
+    playerEntry = leaderboard.find(entry => entry.name === playerName);
+
+    if (playerEntry) {
+        // If the player is already in the leaderboard, increment their score
+        playerEntry.score++;
+    } else {
+        // If the player is not in the leaderboard, add them with a score of 1
+        leaderboard.push({name: playerName, score: 1});
+    }
+
+    // Save the updated leaderboard back to localStorage
+    localStorage.setItem('leaderboard', JSON.stringify(leaderboard));
+    
+    // Check if tableBody is not null before calling displayLeaderboard
+    if (tableBody) {
+        displayLeaderboard();
+    }
+    console.log('Updated leaderboard:', leaderboard);
 }
 //Function to handle the restart button click event
 function onRestart() {
@@ -298,6 +341,9 @@ function resetGame() {
     scoreboard.computer.textContent = "Computer: " + scores.computer;
     // Hide the result element
     resultElement.style.display = 'none';
+    // Reset the player and opponent hand images
+    playerHandImg.src = "./assets/screenshots/moves/emptyhand.png";
+    opponentHandImg.src = "./assets/screenshots/moves/emptyhand.png";
 }
 //Function to handle the mute button click event
 function onMuteBtnClick() {
@@ -318,17 +364,27 @@ function onMuteBtnClick() {
         icon.classList.add('fa-volume-mute');
     }
 }
-
-
+// Function to handle DOMContentLoaded event
+function handleDOMContentLoaded() {
+    // Check if tableBody is not null
+    if (tableBody) {
+        displayLeaderboard();
+    }
+    
+}
 //Event listeners
 
 //Function to initialize event listeners
 function initEventListeners() {
-    // Add a click event listener to the start button
-    startButton.addEventListener('click', onStartBtnClick);    
+    // Check if startButton is not null before adding event listener
+    if (startButton) {
+        startButton.addEventListener('click', onStartBtnClick);
+    }
 
-    // Add a click event listener to the restart button
-    restartButton.addEventListener('click', onRestart);
+    // Check if restartButton is not null before adding event listener
+    if (restartButton) {
+        restartButton.addEventListener('click', onRestart);
+    }
 
     // Add a click event listener to each button
     buttons.forEach(button => {
@@ -337,8 +393,13 @@ function initEventListeners() {
         }
     });
 
-    muteButton.addEventListener('click', onMuteBtnClick);
+    // Check if muteButton is not null before adding event listener
+    if (muteButton) {
+        muteButton.addEventListener('click', onMuteBtnClick);
+    }
+
+    // Add a DOMContentLoaded event listener
+    document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
 }
 
 initEventListeners();
-
