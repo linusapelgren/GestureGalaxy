258b86286f87cda786ea93fef04d4bee2bba2a28
linusapelgren <70014475+linusapelgren@users.noreply.github.com>
2024-04-17 15:36:38 +0200

    Added dropdown manu and style when on small screen

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+let navIcon = document.getElementById("navIcon");
+let dropdownContent = document.getElementById("dropdownContent");
+let icon = navIcon.children[0];
+navIcon.addEventListener("click", navMenuOpen);
+
+function navMenuOpen() {
+    dropdownContent.classList.toggle("active");
+    if (icon.classList.contains("fa-bars")) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+    } else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+    }
+}
